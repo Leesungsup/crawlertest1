21b58fa3f9e2811afe22b2fe0cf1fb00f8f01901
Leesungsup <tmdtjq0116@naver.com>
2021-06-30 18:40:00 +0900

    register

--- member/views.py
+++ member/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from .views import *
+from .models import BoardMember
 # Create your views here.
 def register(request):
-    return render(request,'register.html')
+    if request.method == "GET":
+        return render(request,'register.html')
+    elif request.method == "POST":
+        post=BoardMember()
+        post.username    = request.POST.get('username', None)
+        #print(username)
+        post.password    = request.POST.get('password', None)
+        #print(password)
+        post.re_password = request.POST.get('re_password', None)
+        #print(re_password)
+        post.email  = request.POST.get('email', None)
+        post.save()
+        return render(request, 'register.html')
