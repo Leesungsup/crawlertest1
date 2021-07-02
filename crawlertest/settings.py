d570102c2a1c20f4c211bdfc70a2ba42ba327fa0
Leesungsup <tmdtjq0116@naver.com>
2021-07-02 18:47:07 +0900

    key

--- crawlertest/settings.py
+++ crawlertest/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-rt3lrek!258!9%1lf@=(br*)-2b!fv8r%0mol4g=745np54@4$')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','secret.json')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
